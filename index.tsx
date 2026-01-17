45ebd3c7be17e0a7cd4a532fbd16de1767477fda
Fahmy rosyadi <fahmyrosyadi29@gmail.com>
2026-01-18 02:48:45 +0700

    feat: Integrate Tailwind CSS and update meta tags for portfolio site

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { OSProvider } from './src/context/OSContext';
-import './index.css';
+import './src/index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
